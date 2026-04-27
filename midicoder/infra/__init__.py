4a2aaae3d8d8391cd5bd2aa43d8b4c91aa221cb0
TuTX <tutran@hemidi.com>
2026-04-27 18:02:51 +0700

    P1-012: E18 AWS Terraform Infrastructure Generator

--- midicoder/infra/__init__.py
+++ midicoder/infra/__init__.py
@@ -6,18 +6,25 @@ từ MIR (Midicoder Intermediate Representation).
 
 Các submodules:
 - docker: Docker Compose generation
-- aws: AWS Terraform generation (future)
+- aws: AWS Terraform generation
 
 Sử dụng:
     from midicoder.infra.docker import DockerComposeGenerator
+    from midicoder.infra.aws import TerraformGenerator, AWSInfrastructureConfig
     
-    generator = DockerComposeGenerator()
-    generator.generate(mir, output_path)
+    # Docker Compose
+    docker_gen = DockerComposeGenerator()
+    docker_gen.generate(mir, output_path)
+    
+    # AWS Terraform
+    aws_gen = TerraformGenerator()
+    aws_gen.generate(mir, output_dir)
 
 Author: Midicoder Team
 Version: 1.0.0
 """
 
 from midicoder.infra.docker import DockerComposeGenerator
+from midicoder.infra.aws import AWSInfrastructureConfig, TerraformGenerator
 
-__all__ = ["DockerComposeGenerator"]
+__all__ = ["DockerComposeGenerator", "AWSInfrastructureConfig", "TerraformGenerator"]
