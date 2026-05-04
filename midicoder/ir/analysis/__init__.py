c7fb569c6d954bd5dafd4ad0fe10a25d5c5f9026
TuTX <tutran@hemidi.com>
2026-05-04 14:55:22 +0700

    chore: remove legacy codes

--- midicoder/ir/analysis/__init__.py
+++
@@ -1,4 +0,0 @@
-﻿# analysis
-"""Intent inference and analysis utilities."""
-
-from .intent import *  # noqa: F403
--- midicoder/ir/builder/__init__.py
+++
@@ -1,6 +0,0 @@
-﻿# build
-"""IR build pipeline."""
-
-from .builder import build_ir
-
-__all__ = ["build_ir"]
